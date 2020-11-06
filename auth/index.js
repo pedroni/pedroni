f89fb52f2fd721099d536d6a94d3dcdc6a0e5ca5
pedronidev <lucaspedronid@gmail.com>
2020-11-06 20:09:04 -0300

    helper function to check if user is authenticated

--- auth/index.js
+++ auth/index.js
@@ -22,3 +22,13 @@ export const createUser = async ({email, password}) => {
   return token
 }
 
+export const isAuthenticated = async ({token}) => {
+  token = token.replace('Bearer', '').trim()
+  const database = await databaseConnection()
+  const collection = database.collection('users')
+  const exists = await collection.findOne({token})
+  if (exists) {
+    return true
+  } 
+  return false
+}
