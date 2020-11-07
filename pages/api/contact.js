835ea5887bbdcd33b3d9a006470453f0b31969ee
pedronidev <lucaspedronid@gmail.com>
2020-11-07 19:38:36 -0300

    lint, more data, and role validation on get contacts

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,29 +1,30 @@
-import { isAuthenticated } from "../../auth"
-import { databaseConnection } from "../../database"
+import { last, orderBy } from 'lodash'
+import { isAuthenticated } from '../../auth'
+import { databaseConnection } from '../../database'
 
 const post = async (req, res) => {
   try {
     const database = await databaseConnection()
-    const {name, email, subject} = req.body
+    const { name, email, subject } = req.body
     const collection = database.collection('contact')
-    if(!name) {
+    if (!name) {
       return res.status(400).json({
         message: 'Por gentileza, informe seu nome.'
       })
     }
-    if(!email) {
+    if (!email) {
       return res.status(400).json({
         message: 'Por gentileza, informe seu e-mail.'
       })
     }
-    if(!subject) {
+    if (!subject) {
       return res.status(400).json({
         message: 'Por gentileza, informe sobre o que você gostaria de conversar.'
       })
     }
     const exists = await collection.findOne({
       email
-    });
+    })
     if (exists) {
       await collection.update({
         email
@@ -51,12 +52,12 @@ const post = async (req, res) => {
         ]
       })
     }
-  
+
     return res.status(200).json({
       message: 'Agradeço o contato! Sua mensagem foi enviada. \n Em breve entrarei em contato com você.',
       data: {
-        name, 
-        email, 
+        name,
+        email,
         subject,
         createdAt: new Date()
       }
@@ -71,23 +72,43 @@ const post = async (req, res) => {
 
 export const get = async (req, res) => {
   const isAuth = await isAuthenticated({
-    token: req.headers.authorization
+    token: req.headers.authorization,
+    roles: ['admin']
   })
   if (!isAuth) {
-    return res.status(401).json({message: 'Você precisa estar autenticado para ver essa página.'})
+    return res.status(401).json({ message: 'not enough permissions to see this' })
   }
   const database = await databaseConnection()
   const collection = database.collection('contact')
-  const contacts = await collection.find().sort({'messages.createdAt': -1}).toArray()
-  return res.status(200).json(contacts)
+  const contacts = await collection.find().sort({ 'messages.createdAt': -1 }).toArray()
+  const messages = contacts.reduce((currentMessages, contact) => {
+    contact.messages = orderBy(contact.messages.map(message => ({
+      ...message,
+      email: contact.email
+    })), (message) => new Date(message.createdAt), 'desc')
+
+    return [
+      ...currentMessages,
+      ...(contact.messages || [])
+    ]
+  }, [])
+  const lastMessage = last(orderBy(messages, (message) => new Date(message.createdAt)))
+  return res.status(200).json({
+    data: contacts,
+    statistics: {
+      emailsCount: contacts.length,
+      messagesCount: messages.length,
+      lastMessage
+    }
+  })
 }
 
 export default (req, res) => {
   if (req.method === 'POST') {
-    post(req, res)
+    return post(req, res)
   } else if (req.method === 'GET') {
-    get(req, res)
+    return get(req, res)
   } else {
-    res.status(405)
+    return res.status(405)
   }
 }
