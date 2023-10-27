cac0213f387616be028647dc8a86205cefa5756f
pedroni <pedronidegodois@hotmail.com>
2023-10-27 18:13:04 -0300

    shadow

--- components/clock/Clock.js
+++ components/clock/Clock.js
@@ -22,6 +22,8 @@ export default function Clock() {
         margin: '0 auto',
         fontWeight: '900',
         fontFamily: 'monospace',
+        
+        textShadow: '0 0 1000px white, 0 0 2px white'
       }}
     >
       <div>{dayjs(now).format('HH:mm:ss')}</div>
