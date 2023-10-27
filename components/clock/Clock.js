cec4369befaefbf55d4bf492bfebaf341da523c0
pedroni <pedronidegodois@hotmail.com>
2023-10-27 14:44:54 -0300

    clock

---
+++ components/clock/Clock.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react'
+import styled from 'styled-components'
+import BoxContent from '../BoxContent'
+import dayjs from 'dayjs'
+
+export default function Clock() {
+  const [now, setNow] = useState()
+  useEffect(() => {
+    setInterval(() => {
+      setNow(new Date())
+    }, 1000)
+  }, [])
+
+  return (
+    <div
+      style={{
+        height: 'calc(100vh - 380px)',
+        color: 'var(--color-primary-light)',
+        fontSize: '16vw',
+        textAlign: 'center',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: '0 auto',
+        fontWeight: '900',
+        fontFamily: 'monospace',
+      }}
+    >
+      <div>{dayjs(now).format('HH:mm:ss')}</div>
+    </div>
+  )
+}
---
+++ pages/clock.js
@@ -0,0 +1,10 @@
+import Layout from '../components/Layout'
+import Clock from '../components/clock/Clock'
+
+export default function ClockPage() {
+  return (
+    <Layout>
+      <Clock></Clock>
+    </Layout>
+  )
+}
