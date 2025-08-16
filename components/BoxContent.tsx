b19dab29fa57b8d1226338a4cc4971cbc8e89fe2
pedroni <pedronidegodois@hotmail.com>
2025-08-16 11:04:50 -0300

    move things around

--- components/BoxContent.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const BoxContent = ({ children }) => {
-  return <div className="text-base leading-7">{children}</div>
-}
-
-export default BoxContent
--- components/Table.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import styles from './Table.module.css'
-
-const Table = ({ children, ...props }) => {
-  return <table className={styles.table} {...props}>{children}</table>
-}
-
-export default Table
