3db5a4410bbc5bd92aa7b311bd2b2ff13f87eed7
pedroni <pedronidegodois@hotmail.com>
2025-08-23 19:22:21 -0300

    layout locale app

---
+++ src/app/[locale]/LayoutLocale.tsx
@@ -0,0 +1,20 @@
+import { hasLocale } from 'next-intl'
+import { ReactNode } from 'react'
+import { routing } from '../../i18n/routing'
+import { notFound } from 'next/navigation'
+import { setRequestLocale } from 'next-intl/server'
+
+export default async function LayoutLocale(props: {
+  children: ReactNode
+  params: Promise<Record<string, unknown>>
+}) {
+  const { locale } = await props.params
+  if (!hasLocale(routing.locales, locale)) {
+    notFound()
+  }
+
+  // Enable static rendering
+  setRequestLocale(locale)
+
+  return props.children
+}
